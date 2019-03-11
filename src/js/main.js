eb6e6a2cc0cad307f7eadb7999ef30eeb62278d7
plloyd11 <plloyd11@gmail.com>
2019-03-11 12:21:04 -0400

    major updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,53 @@
-var app = new Vue({
-    el: '#test',
-    data: {
-        baller: 'BALLER',
-        linx: 'https://www.bhaktibarn.com'
-    }
-})
-
 new WOW().init();
+
+Vue.component("Expander", {
+    template: `
+        <div class="Expander">
+            <div class="Expander__trigger" 
+                @click="open=!open" 
+                :class="open?'active':'beforeBorder'">
+                <svg 
+                    class="Expander__trigger-Icon" 
+                    :class="{open:open}" 
+                    width="40" height="12" 
+                    stroke="cornflowerblue">
+                    <polyline points="12,2 20,10 28,2" stroke-width="3" fill="none"></polyline>
+                </svg>
+                {{ title }}
+            </div>
+            <transition :name="animation">
+                <div class="Expander__body" v-show="open">
+                    <slot></slot>
+                </div>
+            </transition>
+        </div>
+    `,
+    props: {
+      title: {
+        type: String,
+        default: 'title'
+      },
+      animation: {
+        type: String,
+        default: 'rightToLeft'
+      }
+    },
+    data() {
+      return {
+        open: false
+      }
+    }
+  })
+  
+  
+  const vm = new Vue({
+    el: ".expanderSection",
+    data: {
+    },
+    computed: {
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  });
--- src/sass/pages/readme.md
+++
@@ -1,3 +0,0 @@
-# Pages
-
-The `./src/sass/pages` folder gathers all page-specific styles. These include stylesheets for the homepage, article pages, contact, about, and other unique page layouts.
