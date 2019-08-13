d70b43f50463b7822efe297a8d6222cb7e2c1c2b
plloyd11 <plloyd11@gmail.com>
2019-08-13 19:17:12 -0400

    rates

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ new WOW().init();
 import TeacherCard from './vue/TeacherCard.js';
 import Expander from './vue/Expander.js';
 import Filter from './filter.js';
-import Faq from './faq.js';
 
 // Mobile Nav
 
