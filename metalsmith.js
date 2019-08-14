6227741fba896941b7b46862a28eb083fd528e88
plloyd11 <plloyd11@gmail.com>
2019-08-14 18:23:24 -0400

    Betsy edits

--- metalsmith.js
+++ metalsmith.js
@@ -8,45 +8,41 @@ const fs = require('fs');
 
 // Metalsmith pipeline
 function metalsmithBuild(callback) {
-  Metalsmith(__dirname)
-  .clean(true)
-  .use(ignore([
-    '.DS_Store',
-    'img/src/**/*',
-    'sass/**/*',
-    'js/src/**/*',
-    'js/app.js'
-  ]))
-  .use(layouts({
-    engine: 'handlebars',
-    directory: './src/templates/',
-    partials: './src/templates/partials/'
-  }))
-  .destination('./build')
-  // Pass in 'true' bool to check debug/logs
-  .use(metalsmithDebug(false))
-  .build((error) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback();
-    }
-  });
+    Metalsmith(__dirname)
+        .clean(true)
+        .use(ignore(['.DS_Store', 'img/src/**/*', 'sass/**/*', 'js/src/**/*', 'js/app.js']))
+        .use(
+            layouts({
+                engine: 'handlebars',
+                directory: './src/templates/',
+                partials: './src/templates/partials/'
+            })
+        )
+        .destination('./build')
+        // Pass in 'true' bool to check debug/logs
+        .use(metalsmithDebug(false))
+        .build(error => {
+            if (error) {
+                callback(error);
+            } else {
+                callback();
+            }
+        });
 }
 
 // Metalsmith debug utility
 function metalsmithDebug(log) {
-  return function(files, metalsmith, callback) {
-    if (log) {
-      console.log('\nMetaData:');
-      console.log(metalsmith.metadata());
-      for (let item in files) {
-        console.log('\nFile:');
-        console.log(files[item]);
-      }
-    }
-    callback();
-  };
+    return function(files, metalsmith, callback) {
+        if (log) {
+            console.log('\nMetaData:');
+            console.log(metalsmith.metadata());
+            for (let item in files) {
+                console.log('\nFile:');
+                console.log(files[item]);
+            }
+        }
+        callback();
+    };
 }
 
 module.exports = metalsmithBuild;
--- src/js/teacher-modals.js
+++
@@ -1,6 +0,0 @@
-new Vue({
-    el: "#nicole",
-    data: {
-        showModal: false
-    }
-});
